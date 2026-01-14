9ab75c16aebc522257794ed6454a1d7d3121f47d
Portega-ort <pemioror@gmail.com>
2026-01-14 10:39:22 -0600

    feat: Implementar lógica de Switch e interfaz CLI para Actividad 1
    - Se agregó la clase `Main` como punto de entrada.
    - Se creó la clase `CLI` en el paquete `ui` para manejar la interacción y mostrar el estado del Switch.
    - Se implementó la clase `Switch` en el paquete `process` con métodos para encender, apagar y verificar el estado.

---
+++ java/src/edu/pedro/ortega/actividad1/Main.java
@@ -0,0 +1,9 @@
+package edu.pedro.ortega.actividad1;
+
+import edu.pedro.ortega.actividad1.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI.start();
+    }
+}
---
+++ java/src/edu/pedro/ortega/actividad1/process/Switch.java
@@ -0,0 +1,20 @@
+package edu.pedro.ortega.actividad1.process;
+
+public class Switch {
+
+    private boolean isOn;
+
+    public void prenderSwitch(){
+        isOn = true;
+    }
+
+    public void apagarSwitch(){
+        isOn = false;
+
+    }
+
+    public boolean isOn(){
+        return this.isOn;
+    }
+
+}
